7f8323a5a2ff97c95972c483c0515104de32e46b
Gabriel Siqueira <gabrielrsiqueira2023@gmail.com>
2025-02-28 15:45:41 -0300

    java/ddd

--- java/ddd/src/main/java/project/productsManagement/domain/management/application/useCases/UserProductUseCase.java
+++ java/ddd/src/main/java/project/productsManagement/domain/management/application/useCases/UserProductUseCase.java
@@ -41,10 +41,6 @@ public class UserProductUseCase {
         return userProductRepository.getByUserId(id);
     }
 
-    public List<UserProduct> getByProductId(Integer id) {
-        return userProductRepository.getByProductId(id);
-    }
-
     public List<UserProduct> getAll() {
         return userProductRepository.getAll();
     }
